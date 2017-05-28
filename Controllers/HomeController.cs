94fd8a3462f29a84b955d0fb6c29e6061fef0470
Kieran Harkin <kieranharkin@googlemail.com>
2017-05-28 20:38:29 +0100

    'NodeServices'

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,8 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.NodeServices;
 
 namespace aspnetcore_angular_sample.Controllers
 {
@@ -13,9 +11,27 @@ namespace aspnetcore_angular_sample.Controllers
             return View();
         }
 
+        public async Task<IActionResult> Chart([FromServices] INodeServices nodeServices) {
+
+            var options = new {
+                width = 400, height = 200,
+                showArea = true, showPoint = true, fullWidth = true
+            };
+            var data = new {
+                labels = new[] {"Mon", "Tue", "Wed", "Thu", "Fri", "Sat"},
+                series = new[] {
+                    new[] {1, 5, 2, 5, 4, 3},
+                    new[] {2, 3, 4, 8, 1, 2},
+                    new[] {5, 4, 3, 2, 1, 0}
+                }
+            };
+            ViewData["ResultsFromNode"] = await nodeServices.InvokeAsync<string>("chartModule.js", "line", options, data);
+            return View();
+        }
+
         public IActionResult Error()
         {
             return View();
         }
-    }
+    }   
 }
